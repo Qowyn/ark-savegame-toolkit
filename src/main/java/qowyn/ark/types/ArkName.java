cf595495a192f343ead58da912b61f3f72cf4f3f
Roland Firmont <qowync@gmail.com>
2016-08-01 21:02:06 +0200

    Make ArkName more sane and optimized

--- src/main/java/qowyn/ark/types/ArkName.java
+++ src/main/java/qowyn/ark/types/ArkName.java
@@ -3,33 +3,36 @@ package qowyn.ark.types;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class ArkName {
+public final class ArkName {
 
   private static final Pattern NAME_INDEX_PATTERN = Pattern.compile("^(.*)_([0-9]+)$");
 
   private final String nameString;
 
   private final int nameIndex;
-
-  private final boolean zeroIndexVisible;
+  
+  private final String string;
 
   public ArkName(String name) {
     Matcher matcher = NAME_INDEX_PATTERN.matcher(name);
     if (matcher.matches()) {
       nameString = matcher.group(1);
       nameIndex = Integer.parseInt(matcher.group(2));
-      zeroIndexVisible = true;
     } else {
       nameString = name;
       nameIndex = 0;
-      zeroIndexVisible = false;
     }
+    string = name;
   }
 
   public ArkName(String nameString, int nameIndex) {
     this.nameString = nameString;
     this.nameIndex = nameIndex;
-    this.zeroIndexVisible = false;
+    if (nameIndex == 0) {
+      string = nameString;
+    } else {
+      string = nameString + "_" + nameIndex;
+    }
   }
 
   public String getNameString() {
@@ -40,51 +43,23 @@ public class ArkName {
     return nameIndex;
   }
 
-  public boolean isZeroIndexVisible() {
-    return zeroIndexVisible;
-  }
-
   @Override
   public String toString() {
-    if (nameIndex == 0 && !zeroIndexVisible) {
-      return nameString;
-    } else {
-      return nameString + "_" + nameIndex;
-    }
+    return string;
   }
 
   @Override
   public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + nameIndex;
-    result = prime * result + ((nameString == null) ? 0 : nameString.hashCode());
-    result = prime * result + (zeroIndexVisible ? 1231 : 1237);
-    return result;
+    return string.hashCode();
   }
 
   @Override
   public boolean equals(Object obj) {
     if (this == obj)
       return true;
-    if (obj == null)
+    if (!(obj instanceof ArkName))
       return false;
-    if (obj instanceof String) {
-      return toString().equals(obj);
-    }
-    if (getClass() != obj.getClass())
-      return false;
-    ArkName other = (ArkName) obj;
-    if (nameIndex != other.nameIndex)
-      return false;
-    if (nameString == null) {
-      if (other.nameString != null)
-        return false;
-    } else if (!nameString.equals(other.nameString))
-      return false;
-    if (zeroIndexVisible != other.zeroIndexVisible)
-      return false;
-    return true;
+    return string.equals(((ArkName)obj).string);
   }
 
 }
